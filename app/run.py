17076ee1cdc0d15ec5e3076edd3c716b7302fad6
JessicaGarre <jessicagarre.m@gmail.com>
2019-08-27 10:25:23 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,10 +1,9 @@
+# Libraries import
 import json
 import plotly
 import pandas as pd
-
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
@@ -26,7 +25,7 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/df.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('disasters_table', engine)
 
 # load model
